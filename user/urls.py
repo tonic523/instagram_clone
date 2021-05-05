90fc09cebc4151ded184cd99ee4268666289eebf
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-05 16:40:23 +0900

    Add: LoginView in user/views.py

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
-from user.views import SignupView
+from user.views import SignupView, LoginView
 urlpatterns = [
-    path('/signup', SignupView.as_view())
+    path('/signup', SignupView.as_view()),
+    path('/login', LoginView.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -38,3 +38,21 @@ class SignupView(View):
             return JsonResponse({'message': 'SUCCESS'})
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        log_email    = data.get('email')
+        log_password = data.get('password')
+
+        if not log_email or not log_password:
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
+        
+        if not User.objects.filter(email=log_email).exists():
+            return JsonResponse({"message":"INVALID_USER"}, status=401)
+
+        if not User.objects.get(email=log_email).password == log_password:
+            return JsonResponse({"message":"INVALID_USER"}, status=401)
+        
+        return JsonResponse({"message":"SUCCESS"}, status=200)
